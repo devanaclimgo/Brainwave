eabe3d7e7a0b23bb592664099e251ec900e58314
Ana <anaclimgo@gmail.com>
2025-04-15 22:06:31 -0300

    button component scope complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-        <Button className='mt-10' href='#login'>
+        <Button className='mt-10'>
           Something
         </Button>
       </div>
