a038a4ed27f359a948161c9eb73efb963ae5fea0
Ana <anaclimgo@gmail.com>
2025-04-16 22:24:47 -0300

    navigation items imported and styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { brainwave } from '../assets'
 
+import { navigation } from '../constants'
+
 const Header = () => {
   return (
     <div className='fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm'>
@@ -11,7 +13,11 @@ const Header = () => {
 
         <nav className='hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent'>
           <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
-            1 2 3
+            {navigation.map((item) => (
+              <a key={item.id} href={item.url} className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}>
+                {item.title}
+              </a>
+            ))}
           </div>
         </nav>
       </div>
