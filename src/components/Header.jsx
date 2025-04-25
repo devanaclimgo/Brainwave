200af54f1a9b30734b2fe0e34151e1d971fe352e
Ana <anaclimgo@gmail.com>
2025-04-24 22:15:48 -0300

    click event navigation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,10 @@ const Header = () => {
     }
   }
 
+  const handleClick = () => {
+    setOpenNavigation(false)
+  }
+
   return (
     <div className={`fixed top-0 left-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'}`}>
       <div className='flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4'>
@@ -48,7 +52,7 @@ const Header = () => {
           Sign in
         </Button>
 
-        <Button className='ml-auto lg:hidden' px='px-3'>
+        <Button className='ml-auto lg:hidden' px='px-3' onClick={toggleNavigation}>
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
