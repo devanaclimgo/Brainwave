43c534c076643554a778e504f71767c2a4c50b44
Ana <anaclimgo@gmail.com>
2025-04-29 18:15:00 -0300

    typo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { brainwave } from '../assets';
 import { navigation } from '../constants';
 import Button from './Button';
 import MenuSvg from '../assets/svg/MenuSvg';
-import HamburgerMenu from './design/Header';
+import { HamburgerMenu } from './design/Header';
 
 const Header = () => {
   const pathname = useLocation();
