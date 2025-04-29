29ae6c23a3e899880ad30ac8d72714203b320fcb
Ana <anaclimgo@gmail.com>
2025-04-29 17:23:38 -0300

    part 1 hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Section from './Section'
+import { curve } from '../assets'
+import Button from './Button'
 
 const Hero = () => {
   return (
@@ -10,7 +12,29 @@ const Hero = () => {
     customPaddings
     id='hero'
   >
-    Hero
+    <div className='container relative'>
+      <div className='relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]'>
+        <h1 className='h1 mb-6'>Explore the Possibilities of AI Chatting with BrainwaveCurve
+          <span className='inline-block relative'>
+            Brainwave{''}
+            <img
+              src={curve}
+              className='absolute top-fullleft-0 w-full xl:-mt-2'
+              width={624}
+              height={28}
+              alt='Curve'
+            />
+          </span>
+        </h1>
+
+        <p className='body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8'>
+          Unleash the power of AI within Brainwave. Ugrade your productivity with Brainwave, the open AI chat app.
+        </p>
+        <Button href='/pricing' white>
+          Get started
+        </Button>
+      </div>
+    </div>
   </Section>
   )
 }
