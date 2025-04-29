a0d3e1d3ae47987db3e019063419e3d5860b28f7
Ana <anaclimgo@gmail.com>
2025-04-29 17:13:17 -0300

    section styled

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import Section from './Section'
 
 const Hero = () => {
-  return <Section>Hero</Section>
+  return (
+  <Section
+    className='pt-[12rem] -mt-[5.25]'
+    crosses
+    crossesOffset='lg:translate-y-[5.25rem]'
+    customPaddings
+    id='hero'
+  >
+    Hero
+  </Section>
+  )
 }
 
 export default Hero
