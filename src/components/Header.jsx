33f9f17f1a7ef024513b683b498e06afc1ad5494
Ana <anaclimgo@gmail.com>
2025-04-29 18:09:00 -0300

    typo fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { disablePageScroll, enablePageScroll } from 'scroll-lock'
 
 import { brainwave } from '../assets';
 import { navigation } from '../constants';
 import Button from './Button';
 import MenuSvg from '../assets/svg/MenuSvg';
-import { HamburgerMenu } from './design/Header';
-import { disablePageScroll, enablePageScroll} from 'scroll-lock'
+import HamburgerMenu from './design/Header';
 
 const Header = () => {
   const pathname = useLocation();
