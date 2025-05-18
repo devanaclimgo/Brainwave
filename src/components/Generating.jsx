0fae4688c98fa8fb2314a35a6cc533662c663471
Ana <anaclimgo@gmail.com>
2025-05-18 13:09:23 -0300

    feat: added classname parameter to div style

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { loading } from '../assets'
 
-const Generating = () => {
+const Generating = ({ className }) => {
   return (
-    <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem]`}>
+    <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${ className || ""} text-base`}>
       <img 
         className='w-5 h-5 mr-4'
         src={loading} alt='Loading'
