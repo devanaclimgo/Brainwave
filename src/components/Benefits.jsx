3fbf87683edefdeb0bb85d0a8ae685ca18e3bf07
Ana <anaclimgo@gmail.com>
2025-05-26 20:21:34 -0300

    feat: heading completed in benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -5,7 +5,10 @@ const Benefits = () => {
   return (
     <Section id='features'>
       <div className='container relative z-2'>
-        
+        <Heading
+          className='md:max-w-md lg:max-w-2xl'
+          title='Chat Smarter, No Harder with Brainwave'
+        />
       </div>
     </Section>
   )
